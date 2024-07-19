98237617b72fe33cd2dd1b6e415149e12343af86
Oumaimaouni1 <oumaima.ouni@keyrus.com>
2024-07-19 07:34:30 +0100

    add docker config

--- ms-order/src/main/java/com/esprit/ms_order/MsOrderApplication.java
+++ ms-order/src/main/java/com/esprit/ms_order/MsOrderApplication.java
@@ -1,13 +1,32 @@
 package com.esprit.ms_order;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableKafka
 public class MsOrderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsOrderApplication.class, args);
 	}
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 
+	@Bean
+	public ObjectMapper objectMapper(){
+		return new ObjectMapper();
+	}
 }
